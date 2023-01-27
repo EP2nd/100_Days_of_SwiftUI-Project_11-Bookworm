1270a19d5f1bf93cdbf0445f44fd38a202f60615
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-01-27 17:43:12 +0100

    Progression

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
     
+    @State private var showingAddScreen = false
     
     var body: some View {
-        
+        NavigationView {
+            List {
+                ForEach(books) { book in
+                    NavigationLink {
+                        DetailView(book: book)
+                    } label: {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            Text(book.title ?? "Unknown title")
+                                .font(.headline)
+                            Text(book.author ?? "Unknown author")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Book Worm")
+        }
     }
 }
 
