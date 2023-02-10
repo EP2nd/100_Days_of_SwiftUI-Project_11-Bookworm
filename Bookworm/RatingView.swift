dde490dbdcc71c8c5d62fcdae8c27ed10a6ac75e
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-02-11 00:05:09 +0100

    Project 15

--- Bookworm/RatingView.swift
+++ Bookworm/RatingView.swift
@@ -33,6 +33,24 @@ struct RatingView: View {
                     .onTapGesture {
                         rating = number
                     }
+                /// Project 15:
+                    /* .accessibilityLabel("\(number == 1 ? "1 star": "\(number) stars")")
+                    .accessibilityRemoveTraits(.isImage)
+                    .accessibilityAddTraits(number > rating ? .isButton : [.isButton, .isSelected]) */
+            }
+        }
+        /// Project 15:
+        .accessibilityElement()
+        .accessibilityLabel("Rating")
+        .accessibilityValue(rating == 1 ? "1 star" : "\(rating) stars")
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                if rating < maximumRating { rating += 1 }
+            case .decrement:
+                if rating > 1 { rating -= 1 }
+            default:
+                break
             }
         }
     }
